17c816071a81885a1bf893e888e051a7ef3d7986
Anmay Gupta <anmaygupta1@gmail.com>
2021-11-08 11:46:33 -0600

    merging

--- sketch.js
+++ sketch.js
@@ -1,11 +1,18 @@
 let absoluteScreenWidth = screen.availWidth;
 let absoluteScreenHeight = screen.availHeight;
 
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Mouse = Matter.Mouse;
+const MouseConstraint = Matter.MouseConstraint;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
 function setup() {
   createCanvas(absoluteScreenWidth, absoluteScreenHeight);
-  
 }
 
 function draw() {
-  background(220);
+  background(0);
 }
